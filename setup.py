2c51344479837cc4df6defe62c1cec8b703a3997
yaojenkuo <tonykuoyj@gmail.com>
2018-04-01 11:43:40 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from distutils.core import setup
+setup(
+    name='casino',
+    packages=['casino'],  # 與前一個值的名稱相同
+    version='0.1',
+    description='A random test lib of a casino',
+    author='Yao Jen Kuo',
+    author_email='tonykuoyj@gmail.com',
+    url='https://github.com/yaojenkuo/casino',
+    #download_url = 'https://github.com/lukeskywalker/mypackage/archive/0.1.tar.gz',
+    keywords=['dice', 'poker', 'casino'],  # arbitrary keywords
+    #classifiers = [],
+)
